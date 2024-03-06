6cce7ce6d8febb51520d7d3598e81c4222b02257
arif <arif@themascoteers.com>
2024-03-06 16:53:08 +1100

    done

--- test_ann_learnable_savi.py
+++ test_ann_learnable_savi.py
@@ -1,13 +1,19 @@
 from ann_learnable_savi import ANNLearnableSAVI
 from ds_manager import DSManager
+import numpy as np
 
 dm = DSManager()
-r2s = []
+accuracies = []
 
 for fold_number, (train_ds, test_ds, validation_ds) in enumerate(dm.get_k_folds()):
     ann = ANNLearnableSAVI(train_ds, test_ds, validation_ds)
     accuracy = ann.run()
-    break
+    accuracies.append(accuracy)
+
+accuracies = np.array(accuracies)
+
+print(np.mean(accuracies))
+print(np.std(accuracies))
 
 
 
--- test_ann_normal_savi.py
+++ test_ann_normal_savi.py
@@ -1,13 +1,19 @@
 from ann_normal_savi import ANNNormalSAVI
 from ds_manager import DSManager
+import numpy as np
 
 dm = DSManager()
-r2s = []
+accuracies = []
 
 for fold_number, (train_ds, test_ds, validation_ds) in enumerate(dm.get_k_folds()):
     ann = ANNNormalSAVI(train_ds, test_ds, validation_ds)
     accuracy = ann.run()
-    break
+    accuracies.append(accuracy)
 
+
+accuracies = np.array(accuracies)
+
+print(np.mean(accuracies))
+print(np.std(accuracies))
 
 
